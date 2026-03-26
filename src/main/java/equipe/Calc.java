8448a1503c9d770977faabfa7035f84b3ae71967
José Vieira <jn038576@gmail.com>
2026-03-25 22:53:19 -0300

    feat: implementar multiplicacao e atualizar lista da equipe

--- src/main/java/equipe/Calc.java
+++ src/main/java/equipe/Calc.java
@@ -8,8 +8,5 @@ public class Calc {
     public int subtracao(int a, int b) { return a - b; }
 
     // Pessoa C
-    public int multiplicacao(int a, int b) {
-        // TODO: implementar
-        return 0; 
-    }
+    public int multiplicacao(int a, int b) { return a * b; }
 }
