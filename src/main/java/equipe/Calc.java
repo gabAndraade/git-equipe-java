cf61a01cc4868e2acd77b10e4735ee66f8d60d6a
José Vieira <jn038576@gmail.com>
2026-03-25 22:40:05 -0300

    feat: corrigindo subtracao

--- src/main/java/equipe/Calc.java
+++ src/main/java/equipe/Calc.java
@@ -5,10 +5,7 @@ public class Calc {
         public int soma(int a, int b) { return a + b; }
 
     // Pessoa B
-    public int subtracao(int a, int b) {
-        // TODO: implementar
-        return 0; 
-    }
+    public int subtracao(int a, int b) { return a - b; }
 
     // Pessoa C
     public int multiplicacao(int a, int b) {
